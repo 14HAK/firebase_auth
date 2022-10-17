8be431c52707e9fea15462ee48601cc3dc12f6f0
Dulon Mahadi <dulonmolla88@gmail.com>
2022-10-17 20:54:32 +0600

    1st: simpole set up install npm and ready project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-6xl bg-red-500'>manager cow maw</h1>
+    </div>
+  );
+}
+
+export default App;
